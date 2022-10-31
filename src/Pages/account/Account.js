99c9be8dcf9a5f72be67012abd3ed1782e3cb683
johnnyblaze993 <107284554+johnnyblaze993@users.noreply.github.com>
2022-10-31 19:07:24 -0400

    figuring out auth

--- src/Pages/account/Account.js
+++ src/Pages/account/Account.js
@@ -1,18 +1,30 @@
 import React from 'react';
-//import auth from FirebaseConfig.js
 
 import { getAuth } from 'firebase/auth';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Paper, Typography } from '@mui/material';
 
 const Account = () => {
   //get the current user
   const auth = getAuth();
   const user = auth.currentUser;
-  console.log(user);
 
   return (
     <Box>
-      <Typography variant="h4"> Hello, {user.displayName} </Typography>
+      <Typography variant="h4">
+        {' '}
+        <br /> {user?.displayName}
+        <Paper
+          sx={{
+            padding: '2rem',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+            width: '100%',
+          }}
+        >
+          <Button variant="contained">Your orders</Button>
+        </Paper>
+      </Typography>
     </Box>
   );
 };
