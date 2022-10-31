a3ba8f0b2e29e4aa9ce2d3e6c9af69920c47fa25
johnnyblaze993 <107284554+johnnyblaze993@users.noreply.github.com>
2022-10-31 11:21:28 -0400

    Login ui and install firebase

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('https://botw-compendium.herokuapp.com/api/v2/all')
+      .then((res) => {
+        setData(res.data);
+      });
+  }, []);
+
+  console.log(data);
+
   return <div>Home</div>;
 };
 
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Paper, Typography } from '@mui/material';
 import React from 'react';
 
 const Login = () => {
@@ -13,42 +13,82 @@ const Login = () => {
         width: '100%',
       }}
     >
-      <Box
+      <Paper
         sx={{
-          width: {
-            xs: '100%',
-            sm: '80%',
-            md: '50%',
-          },
+          padding: '2rem',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          width: '100%',
+          maxWidth: '500px',
         }}
+        elevation={5}
       >
-        <Typography variant="h1">Login</Typography>
-        <Button variant="contained" fullWidth color="primary">
-          Login
-        </Button>
-      </Box>
-
-      <Box
-        sx={{
-          width: {
-            xs: '100%',
-            sm: '80%',
-            md: '50%',
-          },
-        }}
-      >
-        <Typography
+        <Box
           sx={{
-            color: '#fff',
+            width: {
+              xs: '100%',
+              sm: '80%',
+            },
+            margin: '0 auto',
+            textAlign: 'center',
           }}
-          variant="h1"
         >
-          Register
-        </Typography>
-        <Button variant="contained" fullWidth color="primary">
-          Register
-        </Button>
-      </Box>
+          <Typography
+            sx={{
+              fontSize: {
+                xs: '4rem',
+                sm: '5rem',
+              },
+            }}
+            variant="h1"
+          >
+            Login
+          </Typography>
+          <Button variant="contained" fullWidth color="primary">
+            Login
+          </Button>
+        </Box>
+      </Paper>
+
+      <Paper
+        sx={{
+          padding: '2rem',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          width: '100%',
+          maxWidth: '500px',
+        }}
+        elevation={5}
+      >
+        <Box
+          sx={{
+            width: {
+              xs: '100%',
+              sm: '80%',
+            },
+            margin: '0 auto',
+          }}
+        >
+          <Typography
+            sx={{
+              color: '#fff',
+              textAlign: 'center',
+              fontSize: {
+                xs: '3rem',
+                sm: '5rem',
+              },
+            }}
+            variant="h1"
+          >
+            Register
+          </Typography>
+          <Button variant="contained" fullWidth color="primary">
+            Register
+          </Button>
+        </Box>
+      </Paper>
     </Box>
   );
 };
